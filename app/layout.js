fa39c318ac8db48ffc2d6c0b21489ffbf62c9f16
JEaLiFe <83034287+jealife@users.noreply.github.com>
2024-09-05 23:05:15 +0100

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -6,6 +6,15 @@ const inter = Inter({ subsets: ["latin"] });
 export const metadata = {
   title: "Pindi - L'Orchestre Créatif Gabonais",
   description: "Pindi est un orchestre créatif gabonais fondé en 2022 à l'initiative de Khery Seshet3w, Eloquent Grand et de Maître Engone.",
+  openGraph: {
+    icon: "/logo.png",
+    images:
+      "/hero-image.jpeg",
+    url: "https://pindi-officiel.vercel.app/",
+  },
+  twitter: {
+    card: "summary_large_image",
+  },
 };
 
 export default function RootLayout({ children }) {
